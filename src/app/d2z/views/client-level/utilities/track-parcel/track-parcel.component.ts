e9a32021d9321575c96a6e48f175ed6fc8112859
Archanaa <archanaasankar1@gmail.com>
2019-04-02 14:54:08 -0400

    changes for file download

--- src/app/d2z/views/client-level/utilities/track-parcel/track-parcel.component.ts
+++ src/app/d2z/views/client-level/utilities/track-parcel/track-parcel.component.ts
@@ -70,59 +70,72 @@ export class UtilitiesTrackParcel implements OnInit{
     }
 
     downloadTrackingDetails(){
-      let barcodelabelNumber = 'barcodelabelNumber';
-      let referenceNumber = 'referenceNumber';
-      let eventDetails = 'eventDetails';
-      let trackEventDateOccured = 'trackEventDateOccured';
+     this.errorMsg= null;
+      this.successMsg= null;
+      if($("#trackId").val().length >1){
+        this.spinner.show();
+        var trackingData = this.trackParcelForm.value.trackingNumber.split("\n");
+        var result = trackingData.join(',');
+        this.trackingDataService.trackPracel(result, (resp) => {
+          this.spinner.hide();
+          
+          var selectedRows = resp;
+     
 
-      for(var trackingEventsData in this.trackEvents){
-        var trackingData = this.trackEvents[trackingEventsData];
+    
+    if(selectedRows.length > 0 ){
+        var currentTime = new Date();
+        var fileName = '';
+            fileName = "Export_Tracking"+"-"+currentTime.toLocaleDateString();
+          var options = { 
+            fieldSeparator: ',',
+            quoteStrings: '"',
+            decimalseparator: '.',
+            showLabels: true, 
+            useBom: true,
+            headers: [ 'Reference Number',  'BarCode Label Number' ,'Event Time','Event Name']
+
+          };
         
-        for(var trackData in trackingData.trackingEvents){
-          var trackSubData = trackingData.trackingEvents[trackData];
-          console.log(trackSubData)
-
-          // var trackingObj = (
-          //          trackingObj={}, 
-          //          trackingObj[trackingNumber]= trackingData.trackingNumber != null ? invoiceApprovedData.trackingNumber : '' , invoiceApproveObj,
-          //          trackingObj[reference]= trackingData.referenceNuber != null ? invoiceApprovedData.referenceNuber : '', invoiceApproveObj,
-          //          trackingObj[postcode]= trackingData.postcode != null ?  invoiceApprovedData.postcode : '', invoiceApproveObj
-          // );
-
-        }
-
-      }
-
-      var trackingDataList = [];
-    //     let barcodelabelNumber = 'barcodelabelNumber';
-    //     let clearedCustoms = 'clearedCustoms';
-    //     let consignmentCreated = 'consignmentCreated';
-    //     let delivered = 'delivered';
-    //     let eventDetails = 'eventDetails';
-    //     let heldByCustoms = 'heldByCustoms';
-    //     let inTransit = 'inTransit';
-    //     let clearedCustoms = 'processedByFacility';
-    //     let consignmentCreated = 'received';
-    //     let delivered = 'referenceNumber';
-    //     let eventDetails = 'shipmentCreated';
-    //     let heldByCustoms = 'heldByCustoms';
-    //     let inTransit = 'inTransit';
-    //   for(var trackingEvents in this.trackEvents){
-    //     var trackingData = this.trackEvents[trackingEvents];
-    //     var trackingObj = (
-    //       trackingObj={}, 
-    //       trackingObj[trackingNumber]= trackingData.trackingNumber != null ? invoiceApprovedData.trackingNumber : '' , invoiceApproveObj,
-    //       trackingObj[reference]= trackingData.referenceNuber != null ? invoiceApprovedData.referenceNuber : '', invoiceApproveObj,
-    //       trackingObj[postcode]= trackingData.postcode != null ?  invoiceApprovedData.postcode : '', invoiceApproveObj,
-    //       trackingObj[weight]= trackingData.weight != null ? invoiceApprovedData.weight : '', invoiceApproveObj,
-    //       trackingObj[postage]= trackingData.postage != null ? invoiceApprovedData.postage : '', invoiceApproveObj,
-    //       trackingObj[fuelSurcharge]= trackingData.fuelsurcharge != null ? invoiceApprovedData.fuelsurcharge : '', invoiceApproveObj,
-    //       trackingObj[total]= trackingData.total != null ? invoiceApprovedData.total : '', invoiceApproveObj
-    //     );
-    //     trackingDataList.push(invoiceApproveObj);
-    //  };
+           let ReferenceNumber = 'Reference Number';
+           let BarCodeLabelNumber='BarCode Label Number';
+          let EventName = 'Event Name';
+          let EventTime = 'Event Time';
+           var shipmentList = [];
+             for (var importVal in selectedRows) {
+        var adminObj = selectedRows[importVal];
       
-    }
+           for(var i in adminObj.trackingEvents)
+           {
+           var trackdata = adminObj.trackingEvents[i];
+           
+        var importObj = (importObj={}, 
+         importObj[ReferenceNumber]= adminObj.referenceNumber != null ? adminObj.referenceNumber: '', importObj,
+          importObj[BarCodeLabelNumber]= adminObj.barcodelabelNumber != null ? adminObj.barcodelabelNumber: '', importObj,
+          
+          importObj[EventTime]= trackdata.trackEventDateOccured != null ? trackdata.trackEventDateOccured: '', importObj,
+          importObj[EventName]= trackdata.eventDetails != null ? trackdata.eventDetails: '', importObj);
+          console.log(importObj);
+           shipmentList.push(importObj)
+        
+          }
+          }
+       
+        new Angular2Csv(shipmentList, fileName, options);        
+      }else{
+        if(this.englishFlag){
+          this.errorMsg = "**Please select the below records to download the tracking details";
+        }else if(this.chinessFlag){
+          this.errorMsg = "**请选择以下记录以下载跟踪详细信息";
+        }
+      } 
+  
+          setTimeout(() => {this.spinner.hide()}, 5000);
+        })
+      }else{
+        this.errorMsg = this.englishFlag ? '*Please enter the reference number to track the status of the parcel' : '请输入参考编号来追踪包裹的状态';
+      }
+   
 }
 
 export interface TrackEvent {
