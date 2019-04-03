c8accb0f87a0b7f72b6aa6ca86b1980f566e9fdf
“rahameda” <rahameda@paypal.com  git config --global user.email rahameda@paypal.com>
2019-04-04 00:46:46 +0530

    d2z UI Changes

--- src/app/d2z/views/client-level/utilities/track-parcel/track-parcel.component.ts
+++ src/app/d2z/views/client-level/utilities/track-parcel/track-parcel.component.ts
@@ -72,77 +72,42 @@ export class UtilitiesTrackParcel implements OnInit{
     }
 
     downloadTrackingDetails(){
-    
-     this.errorMsg= null;
-      this.successMsg= null;
-      if($("#trackId").val().length >1){
-        this.spinner.show();
-        var trackingData = this.trackParcelForm.value.trackingNumber.split("\n");
-        var result = trackingData.join(',');
-        this.trackingDataService.trackPracel(result, (resp) => {
-          this.spinner.hide();
-          
-          var selectedRows = resp;
-     
+        this.errorMsg= null;
+        this.successMsg= null;
+        var trackingList = [];
+        let ReferenceNumber = 'Reference Number';
+        let BarCodeLabelNumber='BarCode Label Number';
+        let EventName = 'Event Name';
+        let EventTime = 'Event Time';
 
-    
-    if(selectedRows.length > 0 ){
-        var currentTime = new Date();
-        var fileName = '';
-            fileName = "Export_Tracking"+"-"+currentTime.toLocaleDateString();
-          var options = { 
-            fieldSeparator: ',',
-            quoteStrings: '"',
-            decimalseparator: '.',
-            showLabels: true, 
-            useBom: true,
-            headers: [ 'Reference Number',  'BarCode Label Number' ,'Event Time','Event Name']
+        for (var importVal in this.trackEvents) {
+          var adminObj = this.trackEvents[importVal];
+            for(var i in adminObj.trackingEvents){
+            var trackdata = adminObj.trackingEvents[i];
+                var importObj = (
+                importObj={}, 
+                importObj[ReferenceNumber]= adminObj.referenceNumber != null ? adminObj.referenceNumber: '', importObj,
+                importObj[BarCodeLabelNumber]= adminObj.barcodelabelNumber != null ? adminObj.barcodelabelNumber: '', importObj,
+                importObj[EventTime]= trackdata.trackEventDateOccured != null ? trackdata.trackEventDateOccured: '', importObj,
+                importObj[EventName]= trackdata.eventDetails != null ? trackdata.eventDetails: '', importObj);
+                trackingList.push(importObj)
+                }
+            };
 
+            var currentTime = new Date();
+            var fileName = '';
+                fileName = "Export_Tracking"+"-"+currentTime.toLocaleDateString();
+              var options = { 
+                fieldSeparator: ',',
+                quoteStrings: '"',
+                decimalseparator: '.',
+                showLabels: true, 
+                useBom: true,
+                headers: [ 'Reference Number',  'Article Id' ,'Event Time','Event Name']
           };
-        
-           let ReferenceNumber = 'Reference Number';
-           let BarCodeLabelNumber='BarCode Label Number';
-          let EventName = 'Event Name';
-          let EventTime = 'Event Time';
-           var shipmentList = [];
-             for (var importVal in selectedRows) {
-        var adminObj = selectedRows[importVal];
-      
-           for(var i in adminObj.trackingEvents)
-           {
-           var trackdata = adminObj.trackingEvents[i];
-           
-        var importObj = (importObj={}, 
-         importObj[ReferenceNumber]= adminObj.referenceNumber != null ? adminObj.referenceNumber: '', importObj,
-          importObj[BarCodeLabelNumber]= adminObj.barcodelabelNumber != null ? adminObj.barcodelabelNumber: '', importObj,
-          
-          importObj[EventTime]= trackdata.trackEventDateOccured != null ? trackdata.trackEventDateOccured: '', importObj,
-          importObj[EventName]= trackdata.eventDetails != null ? trackdata.eventDetails: '', importObj);
-          console.log(importObj);
-           shipmentList.push(importObj)
-        
-          }
-          }
-       
-        new Angular2Csv(shipmentList, fileName, options);        
-      }else{
-        if(this.englishFlag){
-          this.errorMsg = "**Please select the below records to download the tracking details";
-        }else if(this.chinessFlag){
-          this.errorMsg = "**请选择以下记录以下载跟踪详细信息";
-        }
-      } 
-  
-          setTimeout(() => {this.spinner.hide()}, 5000);
-        })
-      }else{
-        this.errorMsg = this.englishFlag ? '*Please enter the reference number to track the status of the parcel' : '请输入参考编号来追踪包裹的状态';
-      }
-   
-
-
-
+          new Angular2Csv(trackingList, fileName, options);  
     }
+
 }
 
 export interface TrackEvent {
