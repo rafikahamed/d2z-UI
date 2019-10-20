aca801328d652d88fcb7b5ebf3aaf658ce90fa0d
rafikahamed <rafikahamed56@gmail.com>
2019-10-20 09:30:17 +0530

    URL Changes

--- src/app/d2z/service/consignment-upload.service.ts
+++ src/app/d2z/service/consignment-upload.service.ts
@@ -6,8 +6,8 @@ import 'rxjs/add/operator/map';
 import { Subject } from 'rxjs/Subject';
 const hostname = document.location.hostname;
 const apiName = document.location.hostname.includes("speedcouriers.com.au") == true ? "speedcouriers" : "d2z";
-// const baseUrl = "https://"+hostname+"/v1/"+apiName;
-const baseUrl = "http://"+hostname+":8080/v1/"+apiName;
+const baseUrl = "https://"+hostname+"/v1/"+apiName;
+// const baseUrl = "http://"+hostname+":8080/v1/"+apiName;
 //const baseUrl = "http://18.220.140.225:8080/v1/d2z";
 
 @Injectable()
